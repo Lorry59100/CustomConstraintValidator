690d072c556a8a7479fda1b34268ec7ae0ee1cbc
pisha <pisha@hotmail.fr>
2023-07-13 22:39:36 +0200

    register function first init

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AppController extends AbstractController
 {
@@ -13,6 +17,23 @@ class AppController extends AbstractController
     {
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+        ]);
+    }
+
+    #[Route('/register', name: 'register')]
+    public function register(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('app/register.html.twig', [
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
